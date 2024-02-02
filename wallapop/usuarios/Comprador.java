6fb1d4cb3dfc193b11c1fa4999dbd1905115c19c
SrLechuga03 <158574372+SrLechuga03@users.noreply.github.com>
2024-02-02 12:56:25 +0100

    Update Comprador.java

--- wallapop/usuarios/Comprador.java
+++ wallapop/usuarios/Comprador.java
@@ -2,4 +2,37 @@ package wallapop.usuarios;
 
 public class Comprador {
 
+	private String nombre;
+	private int productosComprados[];
+	private double dineroDisponible;
+
+	public Comprador(String nombre, int productosComprados[], double dineroDisponible) {
+		this.nombre = nombre;
+		this.productosComprados = productosComprados;
+		this.dineroDisponible = dineroDisponible;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int[] productosComprados() {
+		return productosComprados;
+	}
+
+	public void setProductosComprados(int productosComprados[]) {
+		this.productosComprados = productosComprados;
+	}
+
+	public double getDineroDisponible() {
+		return dineroDisponible;
+	}
+
+	public void setDineroDisponible(double dineroDisponible) {
+		this.dineroDisponible = dineroDisponible;
+	}
 }
